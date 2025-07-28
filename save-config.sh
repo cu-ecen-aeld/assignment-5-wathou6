37844d7470eae97665dbeabf90159a52da04bbd5
wathou6 <wathou6@users.noreply.github.com>
2025-07-28 02:18:18 -0700

    remove  -j$(nproc)

--- save-config.sh
+++ save-config.sh
@@ -5,12 +5,12 @@
 cd `dirname $0`
 source shared.sh
 mkdir -p base_external/configs/
-make -C buildroot savedefconfig BR2_DEFCONFIG=${AESD_MODIFIED_DEFCONFIG_REL_BUILDROOT} -j$(nproc)
+make -C buildroot savedefconfig BR2_DEFCONFIG=${AESD_MODIFIED_DEFCONFIG_REL_BUILDROOT}
 
 if [ -e buildroot/.config ] && [ -e buildroot/output/build/linux-*/.config ]; then
 	grep "BR2_LINUX_KERNEL_CUSTOM_CONFIG_FILE" buildroot/.config > /dev/null
 	if [ $? -eq 0 ]; then
 		echo "Saving linux defconfig"
-		make -C buildroot linux-update-defconfig -j$(nproc)
+		make -C buildroot linux-update-defconfig
 	fi
 fi
